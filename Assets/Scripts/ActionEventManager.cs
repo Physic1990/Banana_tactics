9c6a2a00d373d5ec56e14dc0881a010a2ef470be
bknopp71 <112128581+bknopp71@users.noreply.github.com>
2023-10-03 16:40:20 -0700

    Update ActionEventManager.cs

--- Assets/Scripts/ActionEventManager.cs
+++ Assets/Scripts/ActionEventManager.cs
@@ -21,7 +21,7 @@ public class actionEvent
          health = 100;
          attack = 50;
       }
-      }
+   }
    // enemy attributes
    private class enemyAttributes{
       public int health;
@@ -30,39 +30,6 @@ public class actionEvent
          health = 100;
          attack = 50;
       }
-   }
+    }
 
-   // Purpose-  will deliver damage of a player and enemy battle
-   // arguments- playerHP - health of the player
-   //            enemyHP - health of the enemy
-   // will update the health with the corresponding damage yielde from a battle
-   // basic weapon attack of 20 hp for a hit at a random attack of 60 perecent accuracy
-
-   public void attackBattle (ref int playerHP, ref int enemyHP){
-   // damage from attack
-   int playerAttackDamage=20;
-   int enemyAttackDamge=20;
-
-   // player was hit
-   if(Random.Range(0, 100) < 60){
-         playerHP=playerHP-enemyAttackDamge;
-   }
-      // enemy was hit
-   if(Random.Range(0, 100) < 60){
-         enemyHP=enemyHP-playerAttackDamage;
-   }
-}
-   
-   // initialize 
-   public actionEvent(){
-   
-   }
-
-   public int getPlayerHealth(){
-      return player.health;
-   }
-
-   public int getEnemyHealth(){
-      return enemy.health;
-   }
 }
