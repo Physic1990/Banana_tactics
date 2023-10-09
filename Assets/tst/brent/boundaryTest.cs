d96f8a91f27aeb7450855fb8f8ff866a0683bad8
bknopp71 <112128581+bknopp71@users.noreply.github.com>
2023-10-08 20:15:35 -0700

    Add files via upload

---
+++ Assets/tst/brent/boundaryTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class NewTestScript
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void Unit_Has_No_Health()
+    {
+        actionEvent lowerBound = new actionEvent();
+        lowerBound.setPlayerHealth(1);
+      
+       Assert.IsTrue(lowerBound.getUpdatePlayerHealth()>0, "Health is at lower bounds, greater than 0");
+      
+    }
+
+    [Test]
+    public void Unit_Health_Is_In_Boundary()
+    {
+        actionEvent lowerBound = new actionEvent();
+        lowerBound.setPlayerHealth(0);
+
+        Assert.IsTrue(lowerBound.getUpdateEnemyHealth()>0, "Health is not in bounds, less than or equal to 0");
+    }
+}
