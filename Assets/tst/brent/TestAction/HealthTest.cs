871a8c901b488163d80b29bd03fabb8c69953a20
bknopp71 <112128581+bknopp71@users.noreply.github.com>
2023-10-15 22:31:55 -0700

    Update HealthTest.cs
    health test update for correct range and no warnings

--- Assets/tst/brent/TestAction/HealthTest.cs
+++ Assets/tst/brent/TestAction/HealthTest.cs
@@ -10,20 +10,17 @@ public class NewTestScript
     [Test]
     public void Unit_Has_No_Health()
     {
-        ActionEventManager lowerBound = new ActionEventManager();
-        lowerBound.setPlayerHealth(1);
-      
-       Assert.IsTrue(lowerBound.getUpdatePlayerHealth()>0, "Health is at lower bounds, greater than 0");
+      var lowerBound = new GameObject().AddComponent<ActionEventManager>();
+      lowerBound.setPlayerHealth(1);
+      Assert.IsTrue(lowerBound.getUpdatePlayerHealth()>0, "Health is at lower bounds, greater than 0");
       
     }
 
     [Test]
     public void Unit_Health_Is_In_Boundary()
     {
-        ActionEventManager lowerBound = new ActionEventManager();
-        lowerBound.setPlayerHealth(0);
-
-        Assert.IsTrue(lowerBound.getUpdateEnemyHealth()>0, "Health is not in bounds, less than or equal to 0");
+      var lowerBound = new GameObject().AddComponent<ActionEventManager>();
+      lowerBound.setPlayerHealth(0);
+      Assert.IsTrue(lowerBound.getUpdatePlayerHealth()<=0 , "Health is not in bounds, less than or equal to 0");
     }
 }
-
