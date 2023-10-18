256a3387468127ed56f457dc1b7ef40f61731fce
dmmiller155 <dami3857@vandals.uidaho.edu>
2023-10-18 09:41:50 -0700

    Added Damien's Boundary Tests

--- Assets/src/damian/AI.cs
+++ Assets/src/damian/AI.cs
@@ -9,31 +9,36 @@ public class AI : MonoBehaviour
 
     public void AITurn(List<GameObject> PlayerUnits, List<GameObject> EnemyUnits)
     {
+
         for (int i = 0; i < EnemyUnits.Count; i++)
         {
-            FindClosestUnit(PlayerUnits, EnemyUnits[i]);
+            int TargetUnit = FindClosestUnit(PlayerUnits, EnemyUnits[i]);
         }
 
 
     }
 
-    private int FindClosestUnit(List<GameObject> PlayerUnits, GameObject EnemyUnit)
+    public int FindClosestUnit(List<GameObject> PlayerUnits, GameObject EnemyUnit)
     {
-            Debug.Log(EnemyUnit);
-            int j = 0;
-            float Mindistance = (Math.Abs(EnemyUnit.transform.position.x - PlayerUnits[j].transform.position.x) + Math.Abs(EnemyUnit.transform.position.y - PlayerUnits[j].transform.position.y));
-            float distance = 0f;
-            int closestUnit = 0;
-            for (j = 1; j < PlayerUnits.Count; j++)
+        Debug.Log(EnemyUnit);
+        int j = 0;
+        float Mindistance = 0f;
+        if (PlayerUnits.Count > 0)
+        {
+            Mindistance = (Math.Abs(EnemyUnit.transform.position.x - PlayerUnits[j].transform.position.x) + Math.Abs(EnemyUnit.transform.position.y - PlayerUnits[j].transform.position.y));
+        }
+        float distance = 0f;
+        int closestUnit = -1;
+        for (j = 1; j < PlayerUnits.Count; j++)
+        {
+            distance = (Math.Abs(EnemyUnit.transform.position.x - PlayerUnits[j].transform.position.x) + Math.Abs(EnemyUnit.transform.position.y - PlayerUnits[j].transform.position.y));
+            if (Mindistance > distance)
             {
-                distance = (Math.Abs(EnemyUnit.transform.position.x - PlayerUnits[j].transform.position.x) + Math.Abs(EnemyUnit.transform.position.y - PlayerUnits[j].transform.position.y));
-                if (Mindistance > distance)
-                {
-                    closestUnit = j;
-                    Mindistance = distance;
-                }
+                closestUnit = j;
+                Mindistance = distance;
             }
-            return closestUnit;
+        }
+        return closestUnit;
 
 
     }
