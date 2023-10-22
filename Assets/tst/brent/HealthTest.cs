81ef7ad41290cec6869b651587f9a19e251502c4
bknopp71 <112128581+bknopp71@users.noreply.github.com>
2023-10-21 22:26:49 -0700

    Add files via upload

---
+++ Assets/tst/brent/HealthTest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class NewTestScript
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void Unit_Health_IsAlive()
+    {
+      var lowerBound = new GameObject().AddComponent<ActionEventManager>();
+      lowerBound.setPlayerHealth(1);
+      Assert.IsTrue(lowerBound.getPlayerEventHealth()>0, "Health is at lower bounds, greater than 0");
+      
+    }
+
+    [Test]
+    public void Unit_Health_IsNotAlive()
+    {
+      var lowerBound = new GameObject().AddComponent<ActionEventManager>();
+      lowerBound.setPlayerHealth(0);
+      Assert.IsTrue(lowerBound.getPlayerEventHealth()<=0 , "Health is not in bounds, less than or equal to 0");
+    }
+}
