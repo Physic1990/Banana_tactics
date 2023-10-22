72f2dd69d91b48e1f46d68f76937c50c0a860f93
bknopp71 <112128581+bknopp71@users.noreply.github.com>
2023-10-21 22:25:09 -0700

    Add files via upload

--- Assets/src/brent/ActionEventManager.cs
+++ Assets/src/brent/ActionEventManager.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 
 public class ActionEventManager : MonoBehaviour
 {
-
-   [SerializeField] UnitAttributes unitAttributes;
-   [SerializeField] UnitAttributes enemyUnitAttributes;
+   //UnitAttributes unitAttributes;
+   //public GameObject attckingUnit;
 
    void Awake()
    {
-      
-   } 
+      //unitAttributes = unit.GetComponent<UnitAttributes>();
+   }
 
    // event running status
    private bool status;
@@ -59,41 +58,32 @@ public class ActionEventManager : MonoBehaviour
    // will update the health with the corresponding damage yielde from a battle
    // basic weapon attack of 20 hp for a hit at a random attack of 60 perecent accuracy
 
-   public void attackBattle (GameObject unit, GameObject enemyUnit){
-
-   unitAttributes = unit.GetComponent<UnitAttributes>();
-   enemyUnitAttributes = enemyUnit.GetComponent<UnitAttributes>();
-
-   double [] playerAtt = unitAttributes.GetAttackStats();
-   double [] enemyAtt = enemyUnitAttributes.GetAttackStats();
-
+   public void attackBattle (){
    // player was hit
    if(Random.Range(0, 100) < 60){
-         unitAttributes.DealDamage((int)enemyAtt[0]);
+         player.health=player.health-enemy.attackDamage;
    }
       // enemy was hit
    if(Random.Range(0, 100) < 60){
-         enemyUnitAttributes.DealDamage((int)playerAtt[0]);
+         enemy.health=enemy.health-player.attackDamage;
    }
-   Debug.Log("Battle");
-   Debug.Log(unitAttributes.GetHealth());
-   Debug.Log(enemyUnitAttributes.GetHealth());
    }
 
    // Purpose-  will deliver damage to a player on movement by terrain
    // will update the health with the corresponding damage yielded from terrain
 
    public void doNothingTurn (GameObject unit){
-      int terrain=5;
-       //GameObject current = unit.GetComponent<UnitAttributes>;
-       unitAttributes = unit.GetComponent<UnitAttributes>();
-        // player was hit 
-       if(Random.Range(0, 100) < 100){
-         unitAttributes.DealDamage(terrain);  
-      } 
-
-      Debug.Log(unitAttributes.GetHealth());
+        // player was hit
+        Debug.Log("No Action taken");
+      /*if(Random.Range(0, 100) < 50){
+         player.health=player.health-terrain;
+      } */
    }
+   
+   // initialize 
+   /*public actionEvent(){
+      terrain = 50;
+   } */
 
    public int getUpdatePlayerHealth(){
       return player.health;
@@ -101,12 +91,6 @@ public class ActionEventManager : MonoBehaviour
 
    public int getUpdateEnemyHealth(){
       return enemy.health;
-   }
-      public void setPlayerHealth(int hp){
-      player.health = hp;
-   }
-   public void setEnemyHealth(int hp){
-      enemy.health = hp;
    }
 }
 
