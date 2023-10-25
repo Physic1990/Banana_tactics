e7fe8fccd06f2b621132dc512a2ae804db50b777
dmmiller155 <dami3857@vandals.uidaho.edu>
2023-10-25 13:52:25 -0700

    Updated my code to standard

--- Assets/src/damian/AI.cs
+++ Assets/src/damian/AI.cs
@@ -7,33 +7,39 @@ public class AI : MonoBehaviour
 {
  
 
-    public void AITurn(List<GameObject> PlayerUnits, List<GameObject> EnemyUnits)
+    public void AITurn(List<GameObject> playerUnits, List<GameObject> enemyUnits)
     {
-        for (int i = 0; i < EnemyUnits.Count; i++)
+        for (int i = 0; i < enemyUnits.Count; i++)
         {
-            int TargetUnit = FindClosestUnit(PlayerUnits, EnemyUnits[i]);
-        }
+            //find the target to move toward or attack
+            int _targetUnit = FindClosestUnit(playerUnits, enemyUnits[i]); 
 
+            //move
+
+            //attack or heal
+
+        }
 
     }
 
-    public int FindClosestUnit(List<GameObject> PlayerUnits, GameObject EnemyUnit)
+    public int FindClosestUnit(List<GameObject> playerUnits, GameObject enemyUnit) //Finds the closest unit to the current controlled unit
     {
         int j = 0;
-        float Mindistance = Mathf.Infinity;
-        float distance = 0f;
-        Debug.Log(PlayerUnits.Count);
-        int closestUnit = -1;
-        for (j = 0; j < PlayerUnits.Count; j++)
+        float _mindistance = Mathf.Infinity;
+        float _distance = 0f;
+        Debug.Log(playerUnits.Count);
+        int _closestUnit = -1;
+        for (j = 0; j < playerUnits.Count; j++)
         {
-            distance = (Math.Abs(EnemyUnit.transform.position.x - PlayerUnits[j].transform.position.x) + Math.Abs(EnemyUnit.transform.position.y - PlayerUnits[j].transform.position.y));
-            if (Mindistance > distance)
+            // |x1-x2| + |y1-y2| distance formula
+            _distance = (Math.Abs(enemyUnit.transform.position.x - playerUnits[j].transform.position.x) + Math.Abs(enemyUnit.transform.position.y - playerUnits[j].transform.position.y)); 
+            if (_mindistance > _distance)
             {
-                closestUnit = j;
-                Mindistance = distance;
+                _closestUnit = j;
+                _mindistance = _distance;
             }
         }
-        return closestUnit;
+        return _closestUnit;
 
 
     }
