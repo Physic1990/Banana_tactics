45222f9935143bc2fddeec6a7e041b0307e1420c
clarclaire <63173280+clarclaire@users.noreply.github.com>
2023-10-25 23:41:42 -0700

    Shashwot Boundary Tests

--- Assets/tst/shashwot/Tiles_test/Tiles_test.cs
+++ Assets/tst/shashwot/Tiles_test/Tiles_test.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
-public class Tiles_test : MonoBehaviour
+public class TileTests : Tile
 {
-    // Start is called before the first frame update
-    void Start()
+    [Test]
+    public void AssignUnit_SetsOccupied()
     {
-        
+        // Arrange
+        GameObject tileObject = new GameObject("Tile");
+        Tile tile = tileObject.AddComponent<Tile>();
+        GameObject unit = new GameObject();
+
+        // Act
+        tile.AssignUnit(unit);
+
+        // Assert
+        Assert.IsTrue(tile._occupied);
     }
 
-    // Update is called once per frame
-    void Update()
+    [Test]
+    public void RemoveUnit_ClearsOccupied()
     {
-        
+        // Arrange
+        GameObject tileObject = new GameObject("Tile");
+        Tile tile = tileObject.AddComponent<Tile>();
+        GameObject unit = new GameObject();
+        tile.AssignUnit(unit);
+
+        // Act
+        GameObject removedUnit = tile.RemoveUnit();
+
+        // Assert
+        Assert.IsFalse(tile._occupied);
+        Assert.IsNull(tile._unit);
     }
 }
