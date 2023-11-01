7fbfc69bd0e44d27d9d55d983f5dfbba53a5df52
shashwot.niraula1990@gmail.com <niraulashashwot@gmail.com>
2023-11-01 16:57:28 -0700

    Added comment to the existing code

--- Assets/src/shashwot/Tile.cs
+++ Assets/src/shashwot/Tile.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    [SerializeField] private Color _baseColor, _offsetColor;
-    [SerializeField] private GameObject _highlight;
-    [SerializeField] public GameObject _unit;
-
-    public bool _occupied = false;
-    private SpriteRenderer _renderer;
+    [SerializeField] private Color _baseColor, _offsetColor;  // Serialized fields for base and offset colors
+    [SerializeField] private GameObject _highlight;  // Serialized field for a highlight GameObject
+    [SerializeField] public GameObject _unit;  // Serialized field for a game unit GameObject
     
 
-    public bool isHighlighted = false;
-    public bool isOccupied = false;
+    public bool _occupied = false;  // Flag indicating if the tile is occupied by a unit
+    private SpriteRenderer _renderer;  // Reference to the SpriteRenderer component
+
+    public bool isHighlighted = false;  // Flag indicating if the tile is currently highlighted
+    public bool isOccupied = false;  // Flag indicating if the tile is occupied (possibly redundant)
+
     private void Awake()
     {
         // Automatically find and assign the SpriteRenderer component
         _renderer = GetComponent<SpriteRenderer>();
     }
+
     public void Init(bool isOffset)
     {
+        // Initialize the tile's color based on whether it's an offset tile or not
         if (isOffset)
         {
             _renderer.color = _offsetColor;
@@ -33,6 +36,7 @@ public class Tile : MonoBehaviour
 
     public void AssignUnit(GameObject passedUnit)
     {
+        // Assign a game unit to the tile, mark it as occupied, and position it at the tile's position
         _occupied = true;
         _unit = passedUnit;
         _unit.transform.position = new Vector2(this.transform.position.x, this.transform.position.y);
@@ -40,6 +44,7 @@ public class Tile : MonoBehaviour
 
     public GameObject RemoveUnit()
     {
+        // Remove the game unit from the tile, mark it as unoccupied, and return the removed unit
         _occupied = false;
         GameObject temp = _unit;
         _unit = null;
@@ -48,30 +53,36 @@ public class Tile : MonoBehaviour
 
     public void SpawnUnit(string tag, List<GameObject> unitLog)
     {
+        // Spawn a game unit with the specified tag on the tile, mark it as occupied, and position it at the tile's position
         _occupied = true;
         _unit = GameObject.FindWithTag(tag);
         _unit.SetActive(true);
         _unit.transform.position = new Vector2(this.transform.position.x, this.transform.position.y);
-        if(tag == "Player")
+        
+        if (tag == "Player")
         {
-            unitLog.Add(_unit);
+            unitLog.Add(_unit);  // If the unit belongs to the player, add it to the unit log (assuming it's a list).
         }
     }
 
     public void TurnOnHighlight()
     {
+        // Turn on the tile's highlight
         _highlight.SetActive(true);
         isHighlighted = true;
     }
 
     public void HighlightNoCursor()
     {
+        // Similar to TurnOnHighlight, but doesn't mention a cursor (possibly a minor variation).
         _highlight.SetActive(true);
         isHighlighted = true;
     }
+
     public void TurnOffHighlight()
     {
-        _highlight?.SetActive(false);
+        // Turn off the tile's highlight if it exists, and mark it as not highlighted
+        _highlight?.SetActive(false);  // The ?. operator ensures null safety
         isHighlighted = false;
     }
 }
