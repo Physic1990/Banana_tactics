dc9bfc1e3630565bb178665c28755d0169dd975d
CalebColeslaw <moua4112@vandals.uidaho.edu>
2023-11-07 18:39:54 -0800

    The Cooler Unit Spawner
    I now made it so that you can just change a .txt file to determine where unit spawns. Check out Level.txt in src/caleb. Each line should be like:
    Tag      Class     X   Y   ID
    (example)
    player | warrior | 0 | 0 | 1
    Spawns a warrior player unit at 0, 0 with id 1.

--- Assets/src/caleb/GridManager.cs
+++ Assets/src/caleb/GridManager.cs
@@ -15,8 +15,6 @@ public class GridManager : MonoBehaviour
     //Features which are initiated one a scene by scene basis
     [SerializeField] private int _width, _height;
     [SerializeField] private Tile _tilePrefab;
-    [SerializeField] private GameObject playerPrefab;
-    [SerializeField] private GameObject enemyPrefab;
     [SerializeField] private Transform _cam;
     //The minimum number of frames that need to pass before a player's next input is read
     [SerializeField] private int _cursorDelay;
@@ -38,9 +36,9 @@ public class GridManager : MonoBehaviour
     //Stores all the generated tiles
     private Dictionary<Vector2, Tile> _tiles;
     //List of all "player" Game Objects
-    private List<GameObject> _playerUnits = new List<GameObject>();
+    public List<GameObject> _playerUnits = new List<GameObject>();
     //List of all "enemy" Game Objects
-    private List<GameObject> _enemyUnits = new List<GameObject>();
+    public List<GameObject> _enemyUnits = new List<GameObject>();
 
     /*************************************************************************
                                 Tile Variables
@@ -136,7 +134,7 @@ public class GridManager : MonoBehaviour
         }
         //The grid is created
         GenerateGrid();
-        GenerateUnits();
+        LevelSetup.Instance.GenerateUnits();
         //The default position of the cursor is always set to the bottom left
         _cursorTile = GetTileAtPosition(new Vector2(0, 0));
         _cursorTile.TurnOnHighlight();
@@ -205,42 +203,6 @@ public class GridManager : MonoBehaviour
         var spawnedTile = Instantiate(_tilePrefab, new Vector3(position.x, position.y, 0), Quaternion.identity);
         spawnedTile.name = $"Tile {position.x} {position.y}";
         return spawnedTile;
-    }
-
-    /*************************************************************************
-              Generates Units on Grid (do not grade)
-    ************************************************************************/
-    public void GenerateUnits()
-    {
-        Tile tile = null;
-        for (int i = 0; i < 5; i++)
-        {
-            tile = GetTileAtPosition(new Vector2(i, 0));
-            SpawnUnitAt(tile, "player", i + 1);
-        }
-        for (int i = 0; i < 5; i++)
-        {
-            tile = GetTileAtPosition(new Vector2(_width - 1 - i, _height - 1));
-            SpawnUnitAt(tile, "enemy", i + 10);
-        }
-    }
-
-    public void SpawnUnitAt(Tile tile, string tag, int id)
-    {
-        if (tag == "player")
-        {
-            var unit = Instantiate(playerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-            tile.AssignUnit(unit);
-            _playerUnits.Add(unit);
-            unit.name = $"{tag} {id}";
-        }
-        else if (tag == "enemy")
-        {
-            var unit = Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-            tile.AssignUnit(unit);
-            _enemyUnits.Add(unit);
-            unit.name = $"{tag} {id}";
-        }
     }
 
     /*************************************************************************
---
+++ Assets/src/caleb/LevelSetup.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSetup : MonoBehaviour
+{
+    public static LevelSetup Instance { get; private set; }
+
+    // Prefabs For Units:
+    [SerializeField] private GameObject warriorPrefab;
+    [SerializeField] private GameObject lancerPrefab;
+    [SerializeField] private GameObject gunslingerPrefab;
+    [SerializeField] private GameObject roguePrefab;
+    [SerializeField] private GameObject heroPrefab;
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private TextAsset textAsset;
+    GridManager grid;
+    private int height;
+    private int width;
+
+    private Dictionary<string, GameObject> classToPrefab; // Map class names to prefabs
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+        grid = GameObject.FindGameObjectWithTag("Grid").GetComponent<GridManager>();
+        height = grid.Height;
+        width = grid.Width;
+
+        // Initialize the class-to-prefab mapping
+        classToPrefab = new Dictionary<string, GameObject>
+        {
+            { "warrior", warriorPrefab },
+            { "lancer", lancerPrefab },
+            { "gunslinger", gunslingerPrefab },
+            { "rogue", roguePrefab },
+            { "hero", heroPrefab }
+        };
+    }
+
+    /*************************************************************************
+      Generates Units on Grid (do not grade)
+    ************************************************************************/
+    public void GenerateUnits()
+    {
+        ReadLevelInfo();
+    }
+
+    public void SpawnUnitAt(string tag, string className, int x, int y, int id)
+    {
+        Tile tile = grid.GetTileAtPosition(new Vector2(x, y));
+        if (tag == "player" && classToPrefab.ContainsKey(className) && tile != null)
+        {
+            GameObject prefab = classToPrefab[className];
+            var unit = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+            tile.AssignUnit(unit);
+            grid._playerUnits.Add(unit);
+            unit.name = $"{tag} {className} {id}";
+        }
+        else if (tag == "enemy" && tile != null)
+        {
+            var unit = Instantiate(enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            tile.AssignUnit(unit);
+            grid._enemyUnits.Add(unit);
+            unit.name = $"{tag} {className} {x}-{y}";
+        }
+        else if(tile == null)
+        {
+            Debug.LogWarning($"Out of Bounds Tile: {x} , {y}");
+        }
+        else
+        {
+            Debug.LogWarning($"Prefab not found for class: {className}");
+        }
+    }
+
+    public void ReadLevelInfo()
+    {
+        if (textAsset != null)
+        {
+            string[] lines = textAsset.text.Split('\n');
+            foreach (string line in lines)
+            {
+                string[] parts = line.Trim().Split('|');
+
+                if (parts.Length == 5)
+                {
+                    string tag = parts[0].Trim();
+                    string classType = parts[1].Trim();
+                    int x, y, id;
+                    if (int.TryParse(parts[2].Trim(), out x) && int.TryParse(parts[3].Trim(), out y) && int.TryParse(parts[4].Trim(), out id))
+                    {
+                        if(x >= width)
+                        {
+                            x = width - 1;
+                        }
+                        else if (x < 0)
+                        {
+                            x = 0;
+                        }
+                        else if (y >= height)
+                        {
+                            y = height - 1;
+                        }
+                        else if (y < height)
+                        {
+                            y = 0;
+                        }
+                        SpawnUnitAt(tag, classType, x, y, id);
+                    }
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("TextAsset is not assigned. Assign a TextAsset in the Inspector.");
+        }
+    }
+}
