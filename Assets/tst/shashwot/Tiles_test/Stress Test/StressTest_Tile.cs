ea0317b47617bae75fa3f308af50740676e75d4b
shashwot.niraula1990@gmail.com <niraulashashwot@gmail.com>
2023-11-09 07:15:35 -0800

    added  debug log in test

--- Assets/tst/shashwot/Tiles_test/Stress Test/StressTest_Tile.cs
+++ Assets/tst/shashwot/Tiles_test/Stress Test/StressTest_Tile.cs
@@ -1,7 +1,6 @@
 /*
-   StressTest_Tile.cs
 
-   This script demonstrates a stress test, designed to assess system performance
+   This script demonstrates a stress test scenario in Unity, designed to assess system performance
    under heavy load. The stress test involves spawning a large number of tiles using the SpawnTiles
    method in a controlled loop specified by the stressTestIterations variable.
 
@@ -25,18 +24,21 @@ public class StressTest_Tile : MonoBehaviour
     public GameObject tilePrefab;
     public int gridSizeX = 10;
     public int gridSizeY = 10;
-    public int stressTestIterations = 100; // Adjust this number. I have crashed my computer many times so you can lower the value. 
+    public int stressTestIterations = 100; // Adjust this number
 
     void Start()
     {
+        Debug.Log("Starting Stress Test...");
         SpawnTiles();
         StressTest();
+        Debug.Log("Stress Test Completed.");
     }
 
-    // Method for spawning tiles in a grid pattern. The SpawnTiles method is responsible for creating and initializing a grid of tiles. 
-    // It uses nested loops to iterate through the specified gridSizeX and gridSizeY, creating a tile GameObject for each grid position.
+    // Method for spawning tiles in a grid pattern
     void SpawnTiles()
     {
+        Debug.Log("Spawning Tiles...");
+
         for (int x = 0; x < gridSizeX; x++)
         {
             for (int y = 0; y < gridSizeY; y++)
@@ -44,25 +46,36 @@ public class StressTest_Tile : MonoBehaviour
                 Vector3 tilePosition = new Vector3(x, y, 0);
                 GameObject newTile = Instantiate(tilePrefab, tilePosition, Quaternion.identity);
                 newTile.GetComponent<Tile>().Init((x + y) % 2 == 0); // Alternate colors based on position
-                Debug.Log("Tile Position: " + newTile.transform.position);
+                Debug.Log($"Tile Spawned at Position: {newTile.transform.position}");
             }
         }
+
+        Debug.Log("Tile Spawning Completed.");
     }
 
     // Method for performing the stress test
     void StressTest()
     {
+        Debug.Log($"Starting Stress Test Iterations ({stressTestIterations} iterations)...");
+
         for (int i = 0; i < stressTestIterations; i++)
         {
             SpawnTiles();
         }
 
+        Debug.Log("Stress Test Iterations Completed.");
+
         // Check and log frame rate after the stress test
         float frameRate = 1.0f / Time.deltaTime;
+        Debug.Log($"Average Frame Rate after Stress Test: {frameRate}");
 
         if (frameRate < 30.0f)
         {
-            Debug.Log("Framerate has dropped below thresh.");
+            Debug.Log("Framerate has dropped below thresh. Consider optimizing your code.");
+        }
+        else
+        {
+            Debug.Log("Framerate is within acceptable range.");
         }
     }
 }
