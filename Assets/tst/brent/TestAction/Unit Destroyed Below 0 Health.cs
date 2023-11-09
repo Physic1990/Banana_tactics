9b0d9f87160d289f67645b11e12fcfa0a474f0ec
bknopp71 <112128581+bknopp71@users.noreply.github.com>
2023-11-08 20:17:08 -0800

    test

--- Assets/tst/brent/TestAction/Unit Destroyed Below 0 Health.cs
+++ Assets/tst/brent/TestAction/Unit Destroyed Below 0 Health.cs
@@ -6,13 +6,17 @@ using UnityEngine.TestTools;
 
 public class UnitDestroyedBelow0Health
 {
-    // A Test behaves as an ordinary method
+
+        // A Test behaves as an ordinary method
     [Test]
     public void Unit_Destroyed_Below_0_HealthTest()
     {
-      var test1 = new GameObject().AddComponent<UnitAttributes>();
-      double [] attributes = test1.GetAttackOneStats();
-      Assert.IsNotNull(attributes[5]);
-      
+      var testGrid = new GameObject().AddComponent<GridManager>();
+      var testUnit = new GameObject().AddComponent<UnitAttributes>();
+      // kill unit
+      testUnit.DealDamage(100);
+      //testGrid.RemoveDeadUnits();
+      int attributes = testUnit.GetHealth();
+      Assert.IsNotNull(attributes);
     }
 }
