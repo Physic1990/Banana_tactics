80538beb362fb66525d6369f595df3aaed43410c
shashwot.niraula1990@gmail.com <niraulashashwot@gmail.com>
2023-11-09 01:40:01 -0800

    Added comment

--- Assets/tst/shashwot/Tiles_test/Stress Test/StressTest_Tile.cs
+++ Assets/tst/shashwot/Tiles_test/Stress Test/StressTest_Tile.cs
@@ -1,3 +1,21 @@
+/*
+   StressTest_Tile.cs
+
+   This script demonstrates a stress test, designed to assess system performance
+   under heavy load. The stress test involves spawning a large number of tiles using the SpawnTiles
+   method in a controlled loop specified by the stressTestIterations variable.
+
+   Adjust the stressTestIterations variable to control the intensity of the stress test. Higher values
+   will put more strain on the system.
+
+   The StressTest method monitors the frame rate during and after the stress test. If the frame rate drops
+   below 30 frames per second, a message is logged to indicate potential performance issues.
+
+   Note: This explicit stress test is intended for assessing how well the system handles the tile spawning
+   operation and identifying any performance bottlenecks.
+
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +25,7 @@ public class StressTest_Tile : MonoBehaviour
     public GameObject tilePrefab;
     public int gridSizeX = 10;
     public int gridSizeY = 10;
-    public int stressTestIterations = 100; // Adjust this number
+    public int stressTestIterations = 100; // Adjust this number. I have crashed my computer many times so you can lower the value. 
 
     void Start()
     {
@@ -15,6 +33,8 @@ public class StressTest_Tile : MonoBehaviour
         StressTest();
     }
 
+    // Method for spawning tiles in a grid pattern. The SpawnTiles method is responsible for creating and initializing a grid of tiles. 
+    // It uses nested loops to iterate through the specified gridSizeX and gridSizeY, creating a tile GameObject for each grid position.
     void SpawnTiles()
     {
         for (int x = 0; x < gridSizeX; x++)
@@ -29,6 +49,7 @@ public class StressTest_Tile : MonoBehaviour
         }
     }
 
+    // Method for performing the stress test
     void StressTest()
     {
         for (int i = 0; i < stressTestIterations; i++)
@@ -36,6 +57,7 @@ public class StressTest_Tile : MonoBehaviour
             SpawnTiles();
         }
 
+        // Check and log frame rate after the stress test
         float frameRate = 1.0f / Time.deltaTime;
 
         if (frameRate < 30.0f)
--- Assets/tst/shashwot/Tiles_test/Tiles_test.cs
+++ Assets/tst/shashwot/Tiles_test/Tiles_test.cs
@@ -1,3 +1,12 @@
+/*
+Boundary Test Explanation:
+
+The TileTests script contains two boundary test methods, AssignUnit_SetsOccupied and RemoveUnit_ClearsOccupied,
+designed to ensure the proper functionality of the AssignUnit and RemoveUnit methods within the Tile class.
+These tests focus on the extreme conditions of assigning and removing units to validate the behavior of the Tile class
+in scenarios where a unit is assigned or removed, verifying that the _occupied flag and _unit field are updated correctly.
+The tests use the NUnit framework to set up, act, and assert the expected outcomes.
+*/
 using NUnit.Framework;
 using UnityEngine;
 
@@ -7,14 +16,19 @@ public class TileTests
     public void AssignUnit_SetsOccupied()
     {
         // Arrange
+        // Creating a GameObject to simulate a Tile in the scene.
         GameObject tileObject = new GameObject("Tile");
+        // Adding the Tile component to the GameObject.
         Tile tile = tileObject.AddComponent<Tile>();
+        // Creating a GameObject to simulate a Unit in the scene.
         GameObject unit = new GameObject();
 
         // Act
+        // Assigning the Unit to the Tile using the AssignUnit method.
         tile.AssignUnit(unit);
 
         // Assert
+        // Verifying that the Tile is marked as occupied after assigning the Unit.
         Assert.IsTrue(tile._occupied);
     }
 
@@ -22,16 +36,23 @@ public class TileTests
     public void RemoveUnit_ClearsOccupied()
     {
         // Arrange
+        // Creating a GameObject to simulate a Tile in the scene.
         GameObject tileObject = new GameObject("Tile");
+        // Adding the Tile component to the GameObject.
         Tile tile = tileObject.AddComponent<Tile>();
+        // Creating a GameObject to simulate a Unit in the scene.
         GameObject unit = new GameObject();
+        // Assigning the Unit to the Tile to simulate an initially occupied Tile.
         tile.AssignUnit(unit);
 
         // Act
+        // Removing the Unit from the Tile using the RemoveUnit method.
         GameObject removedUnit = tile.RemoveUnit();
 
         // Assert
+        // Verifying that the Tile is no longer occupied after removing the Unit.
         Assert.IsFalse(tile._occupied);
+        // Verifying that the _unit field is set to null after removing the Unit.
         Assert.IsNull(tile._unit);
     }
 }
